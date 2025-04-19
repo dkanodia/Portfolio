8bcd6163c59772fb0e6b41847f31e699e5d5e122
dkanodia <dkanodia@ucsd.edu>
2025-04-18 20:02:15 -0700

    push JS

---
+++ global.js
@@ -0,0 +1,89 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+    return Array.from(context.querySelectorAll(selector));
+  }
+
+// var navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+// if (currentLink) {
+//   currentLink.classList.add('current');
+// }
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/" 
+  : "/Portfolio/";   // your GitHub repo name
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume.html', title: 'Resume' },
+  { url: 'https://github.com/dkanodia', title: 'Github' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  // Highlight the current page
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname,
+  );
+
+  a.toggleAttribute(
+    'target',
+    a.host !== location.host
+  );
+
+  nav.append(a);
+}
+
+// Insert Dark Mode Switcher
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+// Function to set color scheme
+function setColorScheme(scheme) {
+  document.documentElement.style.setProperty('color-scheme', scheme);
+}
+
+// Get <select> element
+const select = document.querySelector('.color-scheme select');
+
+// Listen for changes
+select.addEventListener('input', function (event) {
+  const scheme = event.target.value;
+  setColorScheme(scheme);
+  localStorage.colorScheme = scheme;
+});
+
+// On page load, check localStorage
+if ("colorScheme" in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
